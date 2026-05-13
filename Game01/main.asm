3133e017fac9b84b5b160b1c06f9d40f2ac360c2
seb835 <nowt835@gmail.com>
2026-05-13 11:47:23 +0100

    Setup and binary files

--- Game01/main.asm
+++ Game01/main.asm
@@ -5,3 +5,15 @@ BasicUpstart2(main)
 
 *=GAME_CODE_ADDRESS "Game Code"
 #import "gameCode.asm"
+
+*=VARIABLES_ADDRESS "Variables"
+#import "variables.asm"
+
+*=TILESET_ADDRESS "Tileset"
+.import binary "tileset.bin"
+
+*=CHARSET_ATTRIB_ADDRESS "Charset Attrib"
+.import binary "charsetAttrib.bin"
+
+*=HUD_ADDRESS "HUD"
+.import binary "hud.bin"
