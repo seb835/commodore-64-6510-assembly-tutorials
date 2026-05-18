2ddccc71448657a2f8ba442fde1e4dc71b882976
seb835 <nowt835@gmail.com>
2026-05-18 16:21:00 +0100

    Manual position of char A

--- DrawTiles01/includes/gameCode.asm
+++ DrawTiles01/includes/gameCode.asm
@@ -9,6 +9,26 @@ main:
 
 gameSetup:
 
+	ldx #24 // Position at row number
+
+	lda Row_LO,x
+	sta ZP_ROW_LO
+	lda Row_HI,x
+	sta ZP_ROW_HI
+
+	lda Row_Color_LO,x
+	sta ZP_ROW_COLOR_LO
+	lda Row_Color_HI,x
+	sta ZP_ROW_COLOR_HI
+
+	ldy #39 // Position at column number
+
+	lda #1 // Screen code for A
+	sta (ZP_ROW_LO),y
+
+	lda #WHITE
+	sta (ZP_ROW_COLOR_LO),y
+
 
 gameLoop:
 	
